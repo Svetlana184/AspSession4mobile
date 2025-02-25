9e844fe2e3c5a05263233c49fb9a25917ae0587f
Svetlana184 <sveta.kgd.39@mail.com>
2025-02-25 13:14:43 +0200

    Добавьте файлы проекта.

---
+++ AspSession4mobile/Controllers/EventController.cs
@@ -0,0 +1,34 @@
+﻿using AspSession4mobile.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspSession4mobile.Controllers
+{
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private RoadOfRussiaContext db;
+        public EventController(RoadOfRussiaContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        [Route("Events")]
+        public IQueryable GetEvents()
+        {
+            var ev = from e in db.Events
+                     select new
+                     {
+                         e.IdEvent,
+                         e.EventName,
+                         e.TypeOfEvent,
+                         e.EventStatus,
+                         e.EventDescription,
+                         e.DateOfEvent,
+                         EventManagers = db.Employees.FirstOrDefault(p => p.IdEmployee.ToString() == e.EventManagers)!.IdEmployee,
+                         e.TypeOfClass
+                     };
+            return ev.AsQueryable();
+
+        }
+    }
+}
---
+++ AspSession4mobile/Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿using AspSession4mobile.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspSession4mobile.Controllers
+{
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private RoadOfRussiaContext db;
+
+        public NewsController(RoadOfRussiaContext db)
+        {
+            this.db = db;
+        }
+        [Route("News")]
+    }
+}
